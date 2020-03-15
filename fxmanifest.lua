d008a862287e18c0ea037e013128dc1a650d312a
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2020-03-15 16:12:16 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+-- resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
 
 name 'Mythic Framework Notification System Modifier by TaerAttO'
 author 'Alzar - https://github.com/Alzar - https://github.com/MonsterTaerAttO'
